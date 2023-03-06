498376e73226d2a2de78ee4343c920825f6fa404
iz <k.ryoji.74@gmail.com>
2023-03-06 23:02:54 +0900

    ✨ Style links section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import "@/styles/global.css";
 import NoSsr from "@/components/shared/NoSsr";
 import { DynamicVoxel } from "@/components/3d/VoxelTaiwan/dynamic";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -57,13 +58,19 @@ export default function Home() {
             <wbr />
             まだ
             <wbr />
-            道すがらですが、
+            道すがら
+            <wbr />
+            ですが、
             <wbr />
             日々
             <wbr />
             目標に
             <wbr />
-            到達できるよう勤しんでいます。
+            到達
+            <wbr />
+            できるよう
+            <wbr />
+            勤しんでいます。
           </p>
           <Link href="/about" className={styles["description-box__read-more"]}>
             more →
@@ -76,15 +83,41 @@ export default function Home() {
         </div>
       </section>
       <section className={styles["main__links"]}>
-        <h2>Links</h2>
-        <p>この辺でつぶいたり活動したりしています。</p>
-        <div>
-          <Link href="/posts">
-            <p>Posts</p>
-            <p>日々の考えや作ったもの置き場</p>
+        <h2 className={styles["links__title"]}>Links</h2>
+        <p className={styles["links__description"]}>
+          この辺でつぶいたり活動したりしています。
+        </p>
+        <div className={styles["links__tiles"]}>
+          <Link href="/posts" className={styles["tiles__posts"]}>
+            <p className={styles["tiles__posts-title"]}>Posts</p>
+            <p className={styles["tiles__posts-description"]}>
+              日々の
+              <wbr />
+              考えや
+              <wbr />
+              作ったもの
+              <wbr />
+              置き場
+            </p>
           </Link>
-          <a href="https://twitter.com/iz_dot" aria-label="twitter"></a>
-          <a href="https://github.com/RyojiK74" aria-label="github"></a>
+          <a
+            className={styles["tiles__twitter"]}
+            target="_blank"
+            rel="noreferer noopener"
+            href="https://twitter.com/iz_dot"
+            aria-label="twitter"
+          >
+            <Image src="/twitter.svg" alt="twitter" width="36" height="30" />
+          </a>
+          <a
+            className={styles["tiles__github"]}
+            target="_blank"
+            rel="noreferer noopener"
+            href="https://github.com/RyojiK74"
+            aria-label="github"
+          >
+            <Image src="/github.svg" alt="github" width="38" height="38" />
+          </a>
         </div>
       </section>
     </>
