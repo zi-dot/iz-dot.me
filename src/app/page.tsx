8d5c04d8d92d7c7ac901da05a4105187f9388495
iz <k.ryoji.74@gmail.com>
2023-03-06 21:38:49 +0900

    🩹 Use wbr

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,28 +4,66 @@ import "@/styles/global.css";
 import NoSsr from "@/components/shared/NoSsr";
 import { DynamicVoxel } from "@/components/3d/VoxelTaiwan/dynamic";
 import Link from "next/link";
-import clsx from "clsx";
 
 export default function Home() {
   return (
     <>
       <section className={styles["main__top"]}>
         <div className={styles["top__description-box"]}>
-          <h1
-            className={clsx(
-              styles["description-box__title"],
-              styles["description-box__title--appear"]
-            )}
-          >
-            Webが好きです。
+          <h1 className={styles["description-box__title"]}>
+            Webが
+            <wbr />
+            好きです。
           </h1>
           <h2 className={styles["description-box__name"]}>iz_dot / zi</h2>
           <p className={styles["description-box__real-name"]}>Ryoji Kono</p>
           <p className={styles["description-box__description-hello"]}>
-            はじめまして、Webフロントエンド開発者のiz_dotです。たまにziという名前も使ってます。
+            はじめまして、
+            <wbr />
+            Webフロントエンド開発者の
+            <wbr />
+            iz_dotです。
+            <wbr />
+            たまに
+            <wbr />
+            ziと
+            <wbr />
+            いう
+            <wbr />
+            名前も
+            <wbr />
+            使ってます。
           </p>
           <p className={styles["description-box__description-goal"]}>
-            人にとって使いやすくワクワクするUIやグラフィックを設計・実装できる人に憧れています。まだ道すがらですが、日々目標に到達できるよう勤しんでいます。
+            人に
+            <wbr />
+            とって
+            <wbr />
+            使いやすく
+            <wbr />
+            ワクワクする
+            <wbr />
+            UIや
+            <wbr />
+            グラフィックを
+            <wbr />
+            設計・
+            <wbr />
+            実装できる
+            <wbr />
+            人に
+            <wbr />
+            憧れています。
+            <wbr />
+            まだ
+            <wbr />
+            道すがらですが、
+            <wbr />
+            日々
+            <wbr />
+            目標に
+            <wbr />
+            到達できるよう勤しんでいます。
           </p>
           <Link href="/about" className={styles["description-box__read-more"]}>
             more →
