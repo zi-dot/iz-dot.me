32b84e8348604043f30d6e96a2db3c70aed2483e
Ryoji Kono <k.ryoji.74@gmail.com>
2023-03-15 12:48:16 +0900

    🔥 Remove cms content related react components and types

--- src/types/cms.ts
+++ src/types/cms.ts
@@ -16,63 +16,6 @@ export type Blog = {
   id: string;
   title: string;
   content: string;
-  // content: {
-  //   contents: (Text | Block | TextBlock | Image | Embed)[];
-  // };
   eyecatch: EyeCatch;
   category: Category[];
 } & MicroCMSDate;
-
-// export type RichTextEditorTextAttributes = {
-//   // script?: "sub" | "super";
-//   blockquote?: boolean;
-//   // size?: "small" | "large" | "huge";
-//   link?: string;
-//   target?: "_blank";
-//   rel?: "noopener noreferrer";
-//   strike?: boolean;
-//   bold?: boolean;
-// };
-//
-// export type RichTextEditorBlockAttributes = {
-//   list?: "bullet" | "ordered";
-//   "code-block"?: boolean;
-//   header?: 1 | 2 | 3 | 4 | 5;
-// };
-//
-// export type RitchTextEditorImageAttributes = {
-//   width: number;
-//   height: number;
-//   link: string | null;
-//   target: "_blank" | null;
-//   alt: string | null;
-// };
-//
-// export type Text = {
-//   type: "text";
-//   value: string;
-//   attributes?: RichTextEditorTextAttributes;
-// };
-//
-// export type Block = {
-//   type: "block";
-//   value: Text[];
-//   attributes?: RichTextEditorBlockAttributes;
-// };
-//
-// export type TextBlock = {
-//   type: "textBlock";
-//   value: Text[];
-// };
-//
-// export type Image = {
-//   type: "image";
-//   value: string;
-//   attributes: RitchTextEditorImageAttributes;
-// };
-//
-// export type Embed = {
-//   type: "embed";
-//   value: string;
-//   attributes: {};
-// };
