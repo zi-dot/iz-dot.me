ab76bbde861fcd724e7660d1c51884b0b0cf9ac2
Ryoji Kono <k.ryoji.74@gmail.com>
2023-03-15 19:12:21 +0900

    💄 Small style changes

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -6,11 +6,11 @@ import styles from "./index.module.css";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className={styles.body}>
+    <>
       <FontStyle />
       <BaseHeaderClient />
       <main className={styles.main}>{children}</main>
       <BaseFooter />
-    </div>
+    </>
   );
 }
