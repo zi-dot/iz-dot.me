4803d0f92639db6de56f356278dc758b3a70f38a
zi-dot <k.ryoji.74@gmail.com>
2024-04-09 22:00:22 +0900

    remove: remove budoux

--- src/pages/posts/[slug]/index.tsx
+++ src/pages/posts/[slug]/index.tsx
@@ -1,7 +1,6 @@
 import { BaseHead } from "@/components/shared/BaseHead";
 import { getBlog, getBlogs } from "@/lib/cmsClient";
 import { Blog } from "@/types/cms";
-import { loadDefaultJapaneseParser } from "budoux";
 import { MicroCMSContentId, MicroCMSDate } from "microcms-js-sdk";
 import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from "next";
 import { ParsedUrlQuery } from "querystring";
@@ -9,7 +8,6 @@ import { ParsedUrlQuery } from "querystring";
 import styles from "./index.module.css";
 
 import { markdownToHtml } from "@/utils/markdownToHtml";
-import Link from "next/link";
 import Image from "next/image";
 import { formatDate } from "@/utils/formatDate";
 import { PostContent } from "@/components/posts/PostContent";
@@ -39,13 +37,9 @@ export const getStaticProps: GetStaticProps<{
   const post = await getBlog(params?.slug as string);
   if (post === null) throw new Error("Post not found");
 
-  const parser = loadDefaultJapaneseParser();
   const contentHtml = await markdownToHtml(post.content);
-  const contentHtmlWithWbr = contentHtml.replace(/(?<=>).*?(?=<)/g, (match) => {
-    return parser.parse(match).join("<wbr />");
-  });
 
-  post.content = contentHtmlWithWbr;
+  post.content = contentHtml;
 
   return {
     props: {
